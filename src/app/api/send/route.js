0c6caddb598fefb396d8b1d18a7c3d53c40785d7
Parameter4600 <derylviper@gmail.com>
2024-10-10 17:36:19 +0100

    fix

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,29 +1,29 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { NextResponse } from "next/server";
 
-const resend = new Resend("re_EoEmW5Tm_55PBTyoUUdbgSSoeehKvXtjW");
-const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
-  try {
+// const resend = new Resend("re_EoEmW5Tm_55PBTyoUUdbgSSoeehKvXtjW");
+// const fromEmail = process.env.FROM_EMAIL;
+
+// export async function POST(req, res) {
+//   const { email, subject, message } = await req.json();
+//   console.log(email, subject, message);
+//   try {
     
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: [fromEmail, email],
+//       subject: subject,
+//       react: (
+//         <>
+//           <h1>{subject}</h1>
+//           <p>Thank you for contacting us!</p>
+//           <p>New message submitted:</p>
+//           <p>{message}</p>
+//         </>
+//       ),
+//     });
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
